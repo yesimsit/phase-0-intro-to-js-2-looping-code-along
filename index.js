285039c36ba334de23f24b302ae08fdc5cf8bfc5
yesim <sityesim@gmail.com>
2022-04-20 23:09:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+function writeCards(names, event){
+    let messages= [];
+    for ( let i = 0; i < names.length; i++ ) {
+        messages.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+      }
+return messages;
+
+}
+function countDown(x){
+    while (x>0) {
+      console.log(x--);
+    }
+    console.log(x);
+}
